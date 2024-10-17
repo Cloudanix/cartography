c198c512edc222090ddba7dddf685ee8b805d7d6
Purusottam <mpurusottamc@gmail.com>
2024-10-17 12:26:26 -0700

    paginate cloudwatch metrics load

--- cartography/intel/aws/cloudwatch.py
+++ cartography/intel/aws/cloudwatch.py
@@ -10,6 +10,7 @@ from cloudconsolelink.clouds.aws import AWSLinker
 
 from cartography.intel.aws.ec2.util import get_botocore_config
 from cartography.util import aws_handle_regions
+from cartography.util import batch
 from cartography.util import run_cleanup_job
 from cartography.util import timeit
 
@@ -22,20 +23,20 @@ aws_console_link = AWSLinker()
 def get_event_buses(boto3_session: boto3.session.Session, region):
     response = {}
     try:
-        client = boto3_session.client('events', region_name=region, config=get_botocore_config())
+        client = boto3_session.client("events", region_name=region, config=get_botocore_config())
         response = client.list_event_buses()
 
     except ClientError as e:
-        logger.error(f'Failed to call EventBridge list_event_buses for region: {region} - {e}')
+        logger.error(f"Failed to call EventBridge list_event_buses for region: {region} - {e}")
 
-    event_buses = response.get('EventBuses', [])
-    while 'NextToken' in response:
+    event_buses = response.get("EventBuses", [])
+    while "NextToken" in response:
         try:
-            response = client.list_event_buses(NextToken=response['NextToken'])
-            event_buses.extend(response.get('EventBuses', []))
+            response = client.list_event_buses(NextToken=response["NextToken"])
+            event_buses.extend(response.get("EventBuses", []))
 
         except ClientError as e:
-            logger.error(f'Failed to call EventBridge list_event_buses for region {region} - {e}')
+            logger.error(f"Failed to call EventBridge list_event_buses for region {region} - {e}")
 
     return event_buses
 
@@ -44,20 +45,30 @@ def get_event_buses(boto3_session: boto3.session.Session, region):
 def transform_event_buses(buses: List[Dict], region: str) -> List[Dict]:
     event_buses = []
     for event_bus in buses:
-        event_bus['region'] = region
-        event_bus['arn'] = event_bus['Arn']
-        event_bus['consolelink'] = aws_console_link.get_console_link(arn=event_bus['arn'])
+        event_bus["region"] = region
+        event_bus["arn"] = event_bus["Arn"]
+        event_bus["consolelink"] = aws_console_link.get_console_link(arn=event_bus["arn"])
         event_buses.append(event_bus)
 
     return event_buses
 
 
-def load_event_buses(session: neo4j.Session, event_buses: List[Dict], current_aws_account_id: str, aws_update_tag: int) -> None:
+def load_event_buses(
+    session: neo4j.Session,
+    event_buses: List[Dict],
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
     session.write_transaction(_load_event_buses_tx, event_buses, current_aws_account_id, aws_update_tag)
 
 
 @timeit
-def _load_event_buses_tx(tx: neo4j.Transaction, event_buses: List[Dict], current_aws_account_id: str, aws_update_tag: int) -> None:
+def _load_event_buses_tx(
+    tx: neo4j.Transaction,
+    event_buses: List[Dict],
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
     query: str = """
     UNWIND $Records as record
     MERGE (bus:AWSEventBridgeEventBus{id: record.Arn})
@@ -85,7 +96,7 @@ def _load_event_buses_tx(tx: neo4j.Transaction, event_buses: List[Dict], current
 
 @timeit
 def cleanup_event_buses(neo4j_session: neo4j.Session, common_job_parameters: Dict) -> None:
-    run_cleanup_job('aws_import_cloudwatch_event_buses_cleanup.json', neo4j_session, common_job_parameters)
+    run_cleanup_job("aws_import_cloudwatch_event_buses_cleanup.json", neo4j_session, common_job_parameters)
 
 
 @timeit
@@ -93,15 +104,15 @@ def cleanup_event_buses(neo4j_session: neo4j.Session, common_job_parameters: Dic
 def get_event_rules(boto3_session: boto3.session.Session, region, account_id):
     event_rules = []
     try:
-        client = boto3_session.client('events', region_name=region, config=get_botocore_config())
-        paginator = client.get_paginator('list_rules')
+        client = boto3_session.client("events", region_name=region, config=get_botocore_config())
+        paginator = client.get_paginator("list_rules")
 
         page_iterator = paginator.paginate()
         for page in page_iterator:
-            event_rules.extend(page['Rules'])
+            event_rules.extend(page["Rules"])
 
     except ClientError as e:
-        logger.error(f'Failed to call CloudWatch event list_rules: {region} - {e}')
+        logger.error(f"Failed to call CloudWatch event list_rules: {region} - {e}")
     return event_rules
 
 
@@ -110,19 +121,29 @@ def transform_rules(rules: List[Dict], region: str, account_id) -> List[Dict]:
     event_rules = []
     for event_rule in rules:
         console_arn = f"arn:aws:cloudwatch:{region if region else ''}:{account_id if account_id else ''}:rule/{event_rule['Name']}"
-        event_rule['consolelink'] = aws_console_link.get_console_link(arn=console_arn)
-        event_rule['region'] = region
+        event_rule["consolelink"] = aws_console_link.get_console_link(arn=console_arn)
+        event_rule["region"] = region
         event_rules.append(event_rule)
 
     return event_rules
 
 
-def load_event_rules(session: neo4j.Session, event_buses: List[Dict], current_aws_account_id: str, aws_update_tag: int) -> None:
+def load_event_rules(
+    session: neo4j.Session,
+    event_buses: List[Dict],
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
     session.write_transaction(_load_event_rules_tx, event_buses, current_aws_account_id, aws_update_tag)
 
 
 @timeit
-def _load_event_rules_tx(tx: neo4j.Transaction, event_buses: List[Dict], current_aws_account_id: str, aws_update_tag: int) -> None:
+def _load_event_rules_tx(
+    tx: neo4j.Transaction,
+    event_buses: List[Dict],
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
     query: str = """
     UNWIND $Records as record
     MERGE (rule:AWSEventBridgeRule{id: record.Arn})
@@ -161,7 +182,7 @@ def _load_event_rules_tx(tx: neo4j.Transaction, event_buses: List[Dict], current
 
 @timeit
 def cleanup_event_rules(neo4j_session: neo4j.Session, common_job_parameters: Dict) -> None:
-    run_cleanup_job('aws_import_cloudwatch_event_rules_cleanup.json', neo4j_session, common_job_parameters)
+    run_cleanup_job("aws_import_cloudwatch_event_rules_cleanup.json", neo4j_session, common_job_parameters)
 
 
 @timeit
@@ -169,19 +190,19 @@ def cleanup_event_rules(neo4j_session: neo4j.Session, common_job_parameters: Dic
 def get_log_groups(boto3_session: boto3.session.Session, region):
     log_groups = []
     try:
-        client = boto3_session.client('logs', region_name=region, config=get_botocore_config())
-        paginator = client.get_paginator('describe_log_groups')
+        client = boto3_session.client("logs", region_name=region, config=get_botocore_config())
+        paginator = client.get_paginator("describe_log_groups")
 
         page_iterator = paginator.paginate()
         for page in page_iterator:
-            log_groups.extend(page['logGroups'])
+            log_groups.extend(page["logGroups"])
 
     except ClientError as e:
-        if e.response['Error']['Code'] in ["NotFoundException"]:
-            logger.debug(f'Failed to call CloudWatch Logs describe_log_groups: {region} - {e}')
+        if e.response["Error"]["Code"] in ["NotFoundException"]:
+            logger.debug(f"Failed to call CloudWatch Logs describe_log_groups: {region} - {e}")
 
         else:
-            logger.error(f'Failed to call CloudWatch Logs describe_log_groups: {region} - {e}')
+            logger.error(f"Failed to call CloudWatch Logs describe_log_groups: {region} - {e}")
 
     return log_groups
 
@@ -190,31 +211,42 @@ def get_log_groups(boto3_session: boto3.session.Session, region):
 def transform_log_groups(boto3_session: boto3.session.Session, groups: List, region: str) -> List[Dict]:
     log_groups = []
     try:
-        kms_client = boto3_session.client('kms', region_name=region, config=get_botocore_config())
+        kms_client = boto3_session.client("kms", region_name=region, config=get_botocore_config())
         for log_group in groups:
-            log_group['arn'] = log_group['arn']
-            log_group['arn'].replace('/', '$252F')
-            log_group['consolelink'] = aws_console_link.get_console_link(arn=log_group['arn'])
-            log_group['region'] = region
-            if log_group.get('kmsKeyId'):
-                log_group['kms'] = kms_client.describe_key(
+            log_group["arn"] = log_group["arn"]
+            log_group["arn"].replace("/", "$252F")
+            log_group["consolelink"] = aws_console_link.get_console_link(arn=log_group["arn"])
+            log_group["region"] = region
+            if log_group.get("kmsKeyId"):
+                log_group["kms"] = kms_client.describe_key(
                     KeyId=log_group.get(
-                        'kmsKeyId', None,
-                    ).split('/')[1],
-                ).get('KeyMetadata', {})
+                        "kmsKeyId",
+                        None,
+                    ).split("/")[1],
+                ).get("KeyMetadata", {})
             log_groups.append(log_group)
     except ClientError as e:
-        logger.error(f'Failed to call CloudWatch Logs describe_log_groups: {region} - {e}')
+        logger.error(f"Failed to call CloudWatch Logs describe_log_groups: {region} - {e}")
 
     return log_groups
 
 
-def load_log_groups(session: neo4j.Session, log_groups: List[Dict], current_aws_account_id: str, aws_update_tag: int) -> None:
+def load_log_groups(
+    session: neo4j.Session,
+    log_groups: List[Dict],
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
     session.write_transaction(_load_log_groups_tx, log_groups, current_aws_account_id, aws_update_tag)
 
 
 @timeit
-def _load_log_groups_tx(tx: neo4j.Transaction, log_groups: List[Dict], current_aws_account_id: str, aws_update_tag: int) -> None:
+def _load_log_groups_tx(
+    tx: neo4j.Transaction,
+    log_groups: List[Dict],
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
     query: str = """
     UNWIND $Records as record
     MERGE (gr:AWSCloudWatchLogGroup{id: record.arn})
@@ -251,7 +283,7 @@ def _load_log_groups_tx(tx: neo4j.Transaction, log_groups: List[Dict], current_a
 
 @timeit
 def cleanup_log_groups(neo4j_session: neo4j.Session, common_job_parameters: Dict) -> None:
-    run_cleanup_job('aws_import_cloudwatch_log_groups_cleanup.json', neo4j_session, common_job_parameters)
+    run_cleanup_job("aws_import_cloudwatch_log_groups_cleanup.json", neo4j_session, common_job_parameters)
 
 
 @timeit
@@ -259,19 +291,19 @@ def cleanup_log_groups(neo4j_session: neo4j.Session, common_job_parameters: Dict
 def get_metrics(boto3_session: boto3.session.Session, region):
     metrics = []
     try:
-        client = boto3_session.client('cloudwatch', region_name=region, config=get_botocore_config())
-        paginator = client.get_paginator('list_metrics')
+        client = boto3_session.client("cloudwatch", region_name=region, config=get_botocore_config())
+        paginator = client.get_paginator("list_metrics")
 
         page_iterator = paginator.paginate()
         for page in page_iterator:
-            metrics.extend(page['Metrics'])
+            metrics.extend(page["Metrics"])
 
     except ClientError as e:
-        if e.response['Error']['Code'] in ["AccessDenied"]:
-            logger.debug(f'Failed to call CloudWatch list_metrics: {region} - {e}')
+        if e.response["Error"]["Code"] in ["AccessDenied"]:
+            logger.debug(f"Failed to call CloudWatch list_metrics: {region} - {e}")
 
         else:
-            logger.error(f'Failed to call CloudWatch list_metrics: {region} - {e}')
+            logger.error(f"Failed to call CloudWatch list_metrics: {region} - {e}")
 
     return metrics
 
@@ -282,20 +314,32 @@ def transform_metrics(mets: List[Dict], account_id: str, region: str) -> List[Di
     for metric in mets:
         console_arn = f"arn:aws:cloudwatch:{region if region else ''}:{account_id if account_id else ''}:metrics/{metric['MetricName']}"
         # metric['consolelink'] = aws_console_link.get_console_link(console_arn)
-        metric['consolelink'] = ''
-        metric['arn'] = metric['MetricName']
-        metric['region'] = region
+        metric["consolelink"] = ""
+        metric["arn"] = metric["MetricName"]
+        metric["region"] = region
         metrics.append(metric)
 
     return metrics
 
 
 def load_metrics(session: neo4j.Session, metrics: List[Dict], current_aws_account_id: str, aws_update_tag: int) -> None:
-    session.write_transaction(_load_metrics_tx, metrics, current_aws_account_id, aws_update_tag)
+    batch_size = 500
+    page_no = 0
+    for paginated_metrics in batch(metrics, size=batch_size):
+        logger.info(
+            f"Loading metrics {page_no * batch_size} - {page_no * batch_size + batch_size} out of {len(metrics)}",
+        )
+        session.write_transaction(_load_metrics_tx, paginated_metrics, current_aws_account_id, aws_update_tag)
+        page_no += 1
 
 
 @timeit
-def _load_metrics_tx(tx: neo4j.Transaction, metrics: List[Dict], current_aws_account_id: str, aws_update_tag: int) -> None:
+def _load_metrics_tx(
+    tx: neo4j.Transaction,
+    metrics: List[Dict],
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
     query: str = """
     UNWIND $Records as record
     MERGE (metric:AWSCloudWatchMetric{id: record.MetricName})
@@ -323,7 +367,7 @@ def _load_metrics_tx(tx: neo4j.Transaction, metrics: List[Dict], current_aws_acc
 
 @timeit
 def cleanup_metrics(neo4j_session: neo4j.Session, common_job_parameters: Dict) -> None:
-    run_cleanup_job('aws_import_cloudwatch_metrics_cleanup.json', neo4j_session, common_job_parameters)
+    run_cleanup_job("aws_import_cloudwatch_metrics_cleanup.json", neo4j_session, common_job_parameters)
 
 
 @timeit
@@ -331,17 +375,17 @@ def cleanup_metrics(neo4j_session: neo4j.Session, common_job_parameters: Dict) -
 def get_cloudwatch_alarm(boto3_session: boto3.session.Session, region: str) -> List[Dict]:
     alarms = []
     try:
-        client = boto3_session.client('cloudwatch', region_name=region, config=get_botocore_config())
-        paginator = client.get_paginator('describe_alarms')
+        client = boto3_session.client("cloudwatch", region_name=region, config=get_botocore_config())
+        paginator = client.get_paginator("describe_alarms")
 
         page_iterator = paginator.paginate()
         for page in page_iterator:
-            alarms.extend(page['MetricAlarms'])
+            alarms.extend(page["MetricAlarms"])
 
         return alarms
 
     except ClientError as e:
-        logger.error(f'Failed to call CloudWatch describe_alarms: {region} - {e}')
+        logger.error(f"Failed to call CloudWatch describe_alarms: {region} - {e}")
         return alarms
 
 
@@ -349,20 +393,30 @@ def get_cloudwatch_alarm(boto3_session: boto3.session.Session, region: str) -> L
 def transform_alarms(alms: List[Dict], region: str) -> List[Dict]:
     alarms = []
     for alarm in alms:
-        alarm['arn'] = alarm['AlarmArn']
-        alarm['consolelink'] = aws_console_link.get_console_link(arn=alarm['arn'])
-        alarm['region'] = region
+        alarm["arn"] = alarm["AlarmArn"]
+        alarm["consolelink"] = aws_console_link.get_console_link(arn=alarm["arn"])
+        alarm["region"] = region
         alarms.append(alarm)
 
     return alarms
 
 
-def load_cloudwatch_alarm(session: neo4j.Session, alarms: List[Dict], current_aws_account_id: str, aws_update_tag: int) -> None:
+def load_cloudwatch_alarm(
+    session: neo4j.Session,
+    alarms: List[Dict],
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
     session.write_transaction(_load_cloudwatch_alarm_tx, alarms, current_aws_account_id, aws_update_tag)
 
 
 @timeit
-def _load_cloudwatch_alarm_tx(tx: neo4j.Transaction, alarms: List[Dict], current_aws_account_id: str, aws_update_tag: int) -> None:
+def _load_cloudwatch_alarm_tx(
+    tx: neo4j.Transaction,
+    alarms: List[Dict],
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
     query: str = """
     UNWIND $Records as record
     MERGE (alarm:AWSCloudWatchAlarm{id: record.AlarmArn})
@@ -396,7 +450,7 @@ def _load_cloudwatch_alarm_tx(tx: neo4j.Transaction, alarms: List[Dict], current
 
 @timeit
 def cleanup_cloudwatch_alarm(neo4j_session: neo4j.Session, common_job_parameters: Dict) -> None:
-    run_cleanup_job('aws_import_cloudwatch_alarm_cleanup.json', neo4j_session, common_job_parameters)
+    run_cleanup_job("aws_import_cloudwatch_alarm_cleanup.json", neo4j_session, common_job_parameters)
 
 
 @timeit
@@ -404,17 +458,17 @@ def cleanup_cloudwatch_alarm(neo4j_session: neo4j.Session, common_job_parameters
 def get_cloudwatch_flowlogs(boto3_session: boto3.session.Session, region: str) -> List[Dict]:
     flowlogs = []
     try:
-        client = boto3_session.client('ec2', region_name=region, config=get_botocore_config())
-        paginator = client.get_paginator('describe_flow_logs')
+        client = boto3_session.client("ec2", region_name=region, config=get_botocore_config())
+        paginator = client.get_paginator("describe_flow_logs")
 
         page_iterator = paginator.paginate()
         for page in page_iterator:
-            flowlogs.extend(page['FlowLogs'])
+            flowlogs.extend(page["FlowLogs"])
 
         return flowlogs
 
     except ClientError as e:
-        logger.error(f'Failed to call EC2 describe_flow_logs: {region} - {e}')
+        logger.error(f"Failed to call EC2 describe_flow_logs: {region} - {e}")
         return flowlogs
 
 
@@ -422,19 +476,29 @@ def get_cloudwatch_flowlogs(boto3_session: boto3.session.Session, region: str) -
 def transform_flow_logs(logs: List[Dict], current_aws_account_id: str, region: str) -> List[Dict]:
     flowlogs = []
     for flowlog in logs:
-        flowlog['arn'] = f"arn:aws:ec2:{region}:{current_aws_account_id}:vcp-flow-log/{flowlog['FlowLogId']}"
-        flowlog['region'] = region
+        flowlog["arn"] = f"arn:aws:ec2:{region}:{current_aws_account_id}:vcp-flow-log/{flowlog['FlowLogId']}"
+        flowlog["region"] = region
         flowlogs.append(flowlog)
 
     return flowlogs
 
 
-def load_cloudwatch_flowlogs(session: neo4j.Session, flowlogs: List[Dict], current_aws_account_id: str, aws_update_tag: int) -> None:
+def load_cloudwatch_flowlogs(
+    session: neo4j.Session,
+    flowlogs: List[Dict],
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
     session.write_transaction(_load_cloudwatch_flowlogs_tx, flowlogs, current_aws_account_id, aws_update_tag)
 
 
 @timeit
-def _load_cloudwatch_flowlogs_tx(tx: neo4j.Transaction, flowlogs: List[Dict], current_aws_account_id: str, aws_update_tag: int) -> None:
+def _load_cloudwatch_flowlogs_tx(
+    tx: neo4j.Transaction,
+    flowlogs: List[Dict],
+    current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
     query: str = """
     UNWIND $Records as record
     MERGE (log:AWSCloudWatchFlowLog{id: record.arn})
@@ -467,13 +531,17 @@ def _load_cloudwatch_flowlogs_tx(tx: neo4j.Transaction, flowlogs: List[Dict], cu
 
 @timeit
 def cleanup_cloudwatch_flowlogs(neo4j_session: neo4j.Session, common_job_parameters: Dict) -> None:
-    run_cleanup_job('aws_import_cloudwatch_flowlog_cleanup.json', neo4j_session, common_job_parameters)
+    run_cleanup_job("aws_import_cloudwatch_flowlog_cleanup.json", neo4j_session, common_job_parameters)
 
 
 @timeit
 def sync(
-    neo4j_session: neo4j.Session, boto3_session: boto3.session.Session, regions: List[str], current_aws_account_id: str,
-    update_tag: int, common_job_parameters: Dict,
+    neo4j_session: neo4j.Session,
+    boto3_session: boto3.session.Session,
+    regions: List[str],
+    current_aws_account_id: str,
+    update_tag: int,
+    common_job_parameters: Dict,
 ) -> None:
     tic = time.perf_counter()
 
